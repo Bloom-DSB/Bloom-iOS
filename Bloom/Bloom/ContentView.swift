7b95b10d7eca5e1db01e71e26e915461747f8369
thisisdana49 <thisisdana49@gmail.com>
2024-06-05 21:53:05 +0900

    [Chore] 프로젝트 파일 생성 및 타겟 설정

---
+++ Bloom/Bloom/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Bloom
+//
+//  Created by 조다은 on 6/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
