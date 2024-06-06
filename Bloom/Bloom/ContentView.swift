87c9d758b0b400ed7cc83f7a9a318c886d5cbbc8
조다은 <chodaeun@Danibook.local>
2024-06-06 12:46:28 +0900

    [Feat] Font 파일 추가 및 Font 확장 정의

--- Bloom/Bloom/ContentView.swift
+++ Bloom/Bloom/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+                .font(.pretendardBlack(size: 30))
         }
         .padding()
         .background(Colors.pointOrange)
